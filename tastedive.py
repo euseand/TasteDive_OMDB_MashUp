af5b9e860366c53b6f67fbc93d1be5fb5f6f650e
euseand <48668000+euseand@users.noreply.github.com>
2020-11-08 15:50:44 +0300

    Initial commit
    tastedive.py to get data from TasteDive API
    omdb.py to get ratings from OMDB API

---
+++ tastedive.py
@@ -0,0 +1,32 @@
+import requests
+
+def get_movies_from_tastedive(movie_title):
+    url = "https://tastedive.com/api/similar"
+    keys_d = {}
+    keys_d['q'] = movie_title
+    keys_d['type'] = 'movie'
+    keys_d['limit'] = 5
+    try:
+        response = requests.get(url, params=keys_d)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        print ('Http Error: ', errh)
+    except requests.exceptions.ConnectionError as errc:
+        print ('Error Connecting: ', errc)
+    except requests.exceptions.Timeout as errt:
+        print ('Timeout Error: ', errt)
+    except requests.exceptions.RequestException as err:
+        print ('Oops: Something Else', err)
+    return response.json()
+    
+def extract_movie_titles(result_dict):
+    try:
+        titles = [movie['Name'] for movie in result_dict['Similar']['Results']]
+    except KeyError as err:
+        print('Error: ', err)
+    return titles
+
+def get_related_movies(movie_titles_list):
+    #return [extract_movie_titles(get_movies_from_tastedive(movie_title)) for movie_title in movie_titles_list]
+
+print(get_related_movies('Pulp Fiction'))
